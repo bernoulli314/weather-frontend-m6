7782a9a4136e36cebff1215c3bd6d7793d6f7535
bernoulli314 <diego.gonzalez.f@ug.uchile.cl>
2026-08-12 22:36:38 -0400

    rutas para github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DetalleLugarView from '../views/DetalleLugarView.vue'
-
+http://localhost:5173/weather-frontend-m6/
 const routes = [
   {
     path: '/',
@@ -21,7 +21,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('#'),
   routes,
 
   scrollBehavior() {
